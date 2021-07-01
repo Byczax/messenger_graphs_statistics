299ea199c3cf34c7b3f1f68b8369939e6140b309
Maciej Byczko <252747@student.pwr.edu.pl>
2021-07-02 01:07:02 +0200

    added numbers to folders
    Signed-off-by: Maciej Byczko <252747@student.pwr.edu.pl>

--- generate_all.sh
+++ generate_all.sh
@@ -10,6 +10,9 @@ for folder in ./fixed_messages/*;do
     previous=
     current=
     next=
+    number=1
+    mkdir -p "img/${folder##*/}/all/"
+    python3 "./src/main.py" $folder "[2000,01,01]" "[2222,01,01]" "xD" "True" "../img/${folder##*/}/all/"
     for date in 20{19,20,21},{03,10},01;do
         previous=$current
         current=$next
@@ -23,8 +26,9 @@ for folder in ./fixed_messages/*;do
                 semester="lato"
                 fi
             # echo $folder
-            mkdir -p "img/${folder##*/}/$semester-${month_current[0]}"
-            python3 "./src/main.py" $folder "[$current]" "[$next]" "xD" "True" "../img/${folder##*/}/$semester-${month_current[0]}/"
+            mkdir -p "img/${folder##*/}/$number-$semester-${month_current[0]}"
+            python3 "./src/main.py" $folder "[$current]" "[$next]" "xD" "True" "../img/${folder##*/}/$number-$semester-${month_current[0]}/"
+            number=$((number+1))
         fi
     # for date in 20{19,20,21},{01,02,03,04,05,06,07,08,09,10,11,12},01;do
     #     echo $date
