0ede098cbad60b2039b21e75c493a3ffe1618490
Maciej Byczko <252747@student.pwr.edu.pl>
2021-07-01 23:39:23 +0200

    small fix
    Signed-off-by: Maciej Byczko <252747@student.pwr.edu.pl>

--- fix_stupid_facebook_unicode_encoding.py
+++ fix_stupid_facebook_unicode_encoding.py
@@ -13,12 +13,13 @@ def parse_obj(dct):
 def main():
     if len(sys.argv) < 3 or sys.argv[1][0] == '-':
         print("Usage:" + sys.argv[0] + "  <input> <output>")
-    filename_in = sys.argv[1]
-    filename_out = sys.argv[2]
-    with open(filename_in, 'r') as f:
-        string = json.dumps(json.load(f, object_hook=parse_obj), indent=4)
-        with open(filename_out, 'w') as out:
-            print(string, file=out)
+    else:
+        filename_in = sys.argv[1]
+        filename_out = sys.argv[2]
+        with open(filename_in, 'r') as f:
+            string = json.dumps(json.load(f, object_hook=parse_obj), indent=4)
+            with open(filename_out, 'w') as out:
+                print(string, file=out)
 
 
 if __name__ == "__main__":
