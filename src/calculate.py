73a7d3ab1263cf9db20e14b88e5216297911c9d0
Maciej Byczko <byczqq136@gmail.com>
2022-01-28 00:13:55 +0100

    Code refactor
    Signed-off-by: Maciej Byczko <byczqq136@gmail.com>

--- src/calculate.py
+++ src/calculate.py
@@ -1,119 +1,108 @@
-import string
-
+from collections import defaultdict
+from typing import Callable
+from parameters import *
 
 # all messages count
-def message_count(my_files: list, start_date: int, end_date: int) -> dict:
-    my_messages = {}
-    for my_file in my_files:
-        for message in my_file["messages"]:
-            if end_date >= message["timestamp_ms"] >= start_date:
-                if message["sender_name"] not in my_messages:
-                    my_messages[message["sender_name"]] = 0
-                my_messages[message["sender_name"]] += 1
-    return my_messages
+
+
+def message_count(data: Filepart) -> dict:
+    messages = defaultdict(int)
+    for message in data.file["messages"]:
+        if not (data.end_date >= message["timestamp_ms"] >= data.start_date):
+            continue
+        messages[message["sender_name"]] += 1
+    return messages
 
 
 # all emoji that people get
-def emoji_all_count(my_files: list, start_date: int, end_date: int) -> dict:
-    my_messages = {}
-    for my_file in my_files:
-        for message in my_file["messages"]:
-            if "reactions" in message:
-                for _ in message["reactions"]:
-                    if end_date >= message["timestamp_ms"] >= start_date:
-                        if message["sender_name"] not in my_messages:
-                            my_messages[message["sender_name"]] = 0
-                        my_messages[message["sender_name"]] += 1
-    return my_messages
+def emoji_all_count(data: Filepart) -> dict:
+    messages = defaultdict(int)
+    for message in data.file["messages"]:
+        if not (data.end_date >= message["timestamp_ms"] >= data.start_date):
+            continue
+        if "reactions" not in message:
+            continue
+        for _ in message["reactions"]:
+            messages[message["sender_name"]] += 1
+    return messages
 
 
 # all heart emoji that people get
-def emoji_heart_count(my_files: list, start_date: int, end_date: int) -> dict:
-    my_messages = {}
-    for my_file in my_files:
-        for message in my_file["messages"]:
-            if "reactions" in message:
-                for reaction in message["reactions"]:
-                    if "\u2764" == reaction["reaction"]:
-                        if end_date >= message["timestamp_ms"] >= start_date:
-                            if message["sender_name"] not in my_messages:
-                                my_messages[message["sender_name"]] = 0
-                            my_messages[message["sender_name"]] += 1
-    return my_messages
+def emoji_heart_count(data: Filepart) -> dict:
+    messages = defaultdict(int)
+    for message in data.file["messages"]:
+        if not (data.end_date >= message["timestamp_ms"] >= data.start_date):
+            continue
+        if "reactions" not in message:
+            continue
+        for reaction in message["reactions"]:
+            if "\u2764" != reaction["reaction"]:
+                continue
+            messages[message["sender_name"]] += 1
+    return messages
 
 
 # count all emoji used in conversation
-def emoji_count(my_files: list, start_date: int, end_date: int) -> dict:
-    emoji = {}
-    for my_file in my_files:
-        for message in my_file["messages"]:
-            if "reactions" in message:
-                for reaction in message["reactions"]:
-                    if end_date >= message["timestamp_ms"] >= start_date:
-                        if reaction["reaction"] not in emoji:
-                            emoji[reaction["reaction"]] = 0
-                        emoji[reaction["reaction"]] += 1
+def emoji_count(data: Filepart) -> dict:
+    emoji = defaultdict(int)
+    for message in data.file["messages"]:
+        if not (data.end_date >= message["timestamp_ms"] >= data.start_date):
+            continue
+        if "reactions" not in message:
+            continue
+        for reaction in message["reactions"]:
+            emoji[reaction["reaction"]] += 1
     return emoji
 
 
 # find how many each people used given word
-def find_word(my_files: list, start_date: int, end_date: int, word: string) -> dict:
-    my_words = {}
-    for my_file in my_files:
-        for message in my_file["messages"]:
-            if "content" in message:
-                word_count = message["content"].lower().count(word.lower())
-                if word_count > 0:
-                    if end_date >= message["timestamp_ms"] >= start_date:
-                        if message["sender_name"] not in my_words:
-                            my_words[message["sender_name"]] = 0
-                        my_words[message["sender_name"]] += word_count
-    return my_words
+def find_word(data: Filepart, word: str) -> dict:
+    words = defaultdict(int)
+    for message in data.file["messages"]:
+        if not (data.end_date >= message["timestamp_ms"] >= data.start_date):
+            continue
+        if "content" not in message:
+            continue
+        word_count = message["content"].lower().count(word.lower())
+        words[message["sender_name"]] += word_count
+    return words
 
 
 # how many each user reacted to other messages
-def giving_reactions(my_files: list, start_date: int, end_date: int) -> dict:
-    my_messages = {}
-    for my_file in my_files:
-        for message in my_file["messages"]:
-
-            if "reactions" in message:
-                for reaction in message["reactions"]:
-                    if end_date >= message["timestamp_ms"] >= start_date:
-                        if reaction["actor"] not in my_messages:
-                            my_messages[reaction["actor"]] = 0
-                        my_messages[reaction["actor"]] += 1
-
-    return my_messages
+def giving_reactions(data: Filepart) -> dict:
+    messages = defaultdict(int)
+    for message in data.file["messages"]:
+        if not (data.end_date >= message["timestamp_ms"] >= data.start_date):
+            continue
+        if "reactions" not in message:
+            continue
+        for reaction in message["reactions"]:
+            messages[reaction["actor"]] += 1
+    return messages
 
 
-def find_only_exact_same(my_files: list, start_date: int, end_date: int, word: string) -> dict:
-    my_words = {}
-    for my_file in my_files:
-        for message in my_file["messages"]:
-            if "content" in message:
-                if message["content"] == word:
-                    if end_date >= message["timestamp_ms"] >= start_date:
-                        if message["sender_name"] not in my_words:
-                            my_words[message["sender_name"]] = 0
-                        my_words[message["sender_name"]] += 1
-    return my_words
+# find exact words
+def find_only_exact_same(data: Filepart, word: str) -> dict:
+    words = defaultdict(int)
+    for message in data.file["messages"]:
+        if not (data.end_date >= message["timestamp_ms"] >= data.start_date):
+            continue
+        if "content" in message and message["content"] == word:
+            words[message["sender_name"]] += 1
+    return words
 
 
 # calculate radio for given parameters
-def ratio(all_keys: dict, my_messages: dict) -> dict:
-    my_ratio = {}
+def ratio(all_keys: dict, messages: dict) -> dict:
+    ratio = defaultdict(int)
+    mul = 10000
     user_names = all_keys.keys()
     for name in user_names:
-        if my_messages[name] and my_messages[name] >= 100:
-            my_ratio[name] = round(all_keys[name] * 10000 / my_messages[name]) / 10000
-    return my_ratio
+        if messages[name] and messages[name] >= 100:
+            ratio[name] = round(all_keys[name] * mul / messages[name]) / mul
+    return ratio
 
 
-# oldest message in files
-def find_the_oldest(my_files: list) -> int:
-    timestamps = []
-    for my_file in my_files:
-        for message in my_file["messages"]:
-            timestamps.append(message["timestamp_ms"])
-    return min(timestamps)
+# message time in files
+def find_time(file: dict, comp: Callable = min) -> int:
+    return comp([message["timestamp_ms"] for message in file["messages"]])
