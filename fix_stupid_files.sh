a2bedffc1e15f97bcea04aa384112bfa48ad0e2d
Maciej Byczko <byczqq136@gmail.com>
2022-01-28 00:15:25 +0100

    Add async
    Signed-off-by: Maciej Byczko <byczqq136@gmail.com>

--- fix_stupid_files.sh
+++ fix_stupid_files.sh
@@ -2,11 +2,13 @@
 
 mkdir -p fixed_messages
 # iterate every folder in messages
-for folder in ./messages/*;do
+for folder in ./messages/*;do {
     mkdir -p "fixed_messages/${folder##*/}"
     # iterate every file in given folder
-    for file in $folder/*;do
-        printf -- "$file --- ./fixed_${file#*/}\n"
-        python3 "./fix_stupid_facebook_unicode_encoding.py" $file "./fixed_${file#*/}"
+    for file in "$folder"/*;do
+        printf -- "%s--- ./fixed_%s\n" "$file" "${file#*/}"
+        python3 "./fix_stupid_facebook_unicode_encoding.py" "$file" "./fixed_${file#*/}"
     done
+}&
 done
+wait
