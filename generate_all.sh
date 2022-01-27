6668ae2c0bebc52705402e7090f644364bb6e850
Maciej Byczko <byczqq136@gmail.com>
2022-01-28 00:16:20 +0100

    Add async + good sheelcheck
    Signed-off-by: Maciej Byczko <byczqq136@gmail.com>

--- generate_all.sh
+++ generate_all.sh
@@ -4,35 +4,25 @@ mkdir -p img
 # iterate every folder in messages
 for folder in ./fixed_messages/*;do
     mkdir -p "img/${folder##*/}"
-    # year=2019
-    # month=10
-    # day=1
-    previous=
     current=
     next=
     number=1
     mkdir -p "img/${folder##*/}/all/"
-    python3 "./src/main.py" $folder "[2000,01,01]" "[2222,01,01]" "xD" "True" "../img/${folder##*/}/all/"
-    for date in 20{18,19,20,21},{03,10},01;do
-        previous=$current
+    python3 "./src/main.py" "$folder" "[2000,01,01]" "[2222,01,01]" "xD" "True" "../img/${folder##*/}/all/" &
+    for date in 20{18,19,20,21,22},{03,10},01;do
         current=$next
         next=$date
-        # echo $current $next
-        if [ -n "$next" -a -n "$current" ];then
-            month_next=($(echo $next | tr "," "\n"))
-            month_current=($(echo $current | tr "," "\n"))
+        if [[ -n "$next" && -n "$current" ]];then
+            mapfile -t month_next < <(echo "$next" | tr "," "\n")
+            mapfile -t month_current < <(echo "$current" | tr "," "\n")
             semester="zima"
-            if [ ${month_next[0]} -eq ${month_current[0]} ];then
+            if [ "${month_next[0]}" -eq "${month_current[0]}" ];then
                 semester="lato"
-                fi
-            # echo $folder
+            fi
             mkdir -p "img/${folder##*/}/$number-$semester-${month_current[0]}"
-            python3 "./src/main.py" $folder "[$current]" "[$next]" "xD" "True" "../img/${folder##*/}/$number-$semester-${month_current[0]}/"
+            python3 "./src/main.py" "$folder" "[$current]" "[$next]" "xD" "True" "../img/${folder##*/}/$number-$semester-${month_current[0]}/" &
             number=$((number+1))
         fi
-    # for date in 20{19,20,21},{01,02,03,04,05,06,07,08,09,10,11,12},01;do
-    #     echo $date
-        # printf -- "python3 ./src/main.py $folder "
     done
-    # echo
 done
+wait
